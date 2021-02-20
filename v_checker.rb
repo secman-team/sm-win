66be46300c2dbb9c37fabf186699129e5c2e5e97
abdfnx <abdfnabd@gmail.com>
2021-02-20 21:39:33 +0300

    v_checher

---
+++ v_checker.rb
@@ -0,0 +1,26 @@
+require "optparse"
+require "colorize"
+
+$l = `verx secman-team/secman -l`
+$c = `secman verx`
+
+def _n()
+  ly = $l.cyan.bold
+  nr = "there's a new release of secman is avalaible:".yellow
+  up = "to update it run".yellow
+  smu = "secman upd".blue
+  scoop = "scoop upgrade secman".cyan
+  puts new_r = "#{nr} #{ly}#{up} go to https://github.com/secman-team/secman/releases\nor if you install secman by scoop run #{scoop}"
+end
+
+def check()
+  if $l != $c
+    _n
+  end
+end
+
+OptionParser.new do |opts|
+  opts.on("-c", "--check", "check the version") do |c|
+    check()
+  end
+end.parse!
